48fa04c9a788fd126c22d12e9b4eaaa6c2de576c
ridwanalmahmud <ridwanalmahmud107@gmail.com>
2025-10-10 18:34:54 +0000

    ncurses starter

---
+++ include/main.h
@@ -0,0 +1,9 @@
+#ifndef __MAIN_H__
+#define __MAIN_H__
+
+#define BACKSPACE 263
+
+void backspace(void);
+void mode_stat(void);
+
+#endif // !__MAIN_H__
--- src/main.c
+++ src/main.c
@@ -1,10 +1,52 @@
+#include "main.h"
 #include <ncurses.h>
+
+void backspace(void) {
+    int x, y;
+    getyx(stdscr, y, x);
+
+    if (x != 0) {
+        move(y, x - 1);
+        delch();
+    } else if (y > 0) {
+        move(y - 1, 0);
+        while (x < COLS && mvinch(y - 1, x) != ' ') {
+            x++;
+        }
+        if (x > 0) {
+            move(y - 1, x - 1);
+        }
+        delch();
+    }
+}
+
+void mode_stat(void) {
+    int row, col;
+    getmaxyx(stdscr, row, col);
+    mvprintw(row - 1, 0, "INSERT");
+    move(0, 0);
+}
 
 int main(void) {
     initscr();
-    printw("Hello World!");
+    raw();
+    keypad(stdscr, true);
+    noecho();
+
+    mode_stat();
+
+    int ch = getch();
+    addch(ch);
+    while (ch != 'q') {
+        ch = getch();
+        if (ch == BACKSPACE) {
+            backspace();
+        } else {
+            addch(ch);
+        }
+    }
+
     refresh();
-    getch();
     endwin();
 
     return 0;
